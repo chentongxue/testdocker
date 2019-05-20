36263b29c093ad9287ada5c5d808876b3eb5a586
chunkschen <suzaku@suzaku.me>
2019-05-20 17:09:25 +0800

    1

--- Dockerfile
+++ Dockerfile
@@ -3,3 +3,4 @@ COPY . /app
 WORKDIR /app
 COPY pip.conf /etc/pip.conf
 RUN pip install -r requirments.txt
+CMD gunicorn -c gunicorn.conf app:app
--- app.py
+++ app.py
@@ -5,12 +5,6 @@ app = Flask(__name__)
 app.debug = True
 
 
-if __name__ != '__main__':
-    gunicorn_logger = logging.getLogger('gunicorn.info')
-    app.logger.handlers = gunicorn_logger.handlers
-    app.logger.setLevel(gunicorn_logger.level)
-
-
 @app.route('/')
 def hello_world():
     return 'Hello World!'
